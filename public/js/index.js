593d862246355bd620cf07e11c6d850886329249
vlatkotodorovski <vlatko_ku@hotmail.com>
2019-02-18 01:01:05 +0100

    Add newMessage and create Message events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Vlatko',
+        text: 'Yup, that works for me'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+})
+
+socket.on('newMessage', function(message){
+    console.log('newMessage', message);
+})
+
